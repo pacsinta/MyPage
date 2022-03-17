bd38d573ab4bcac78012d74217e4c978605414b6
Patrik Csikos <pacsinta@gmail.com>
2022-03-17 19:46:11 +0100

    ssl cert auth file added

--- src/main/kotlin/com/cstcompany/plugins/Routing.kt
+++ src/main/kotlin/com/cstcompany/plugins/Routing.kt
@@ -1,10 +1,8 @@
 package com.cstcompany.plugins
 
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 import java.io.File
 
 fun Application.configureRouting() {
@@ -13,6 +11,12 @@ fun Application.configureRouting() {
         get("/") {
             val index = File("index.html")
             call.respondFile(index)
+        }
+
+        get("/.well-known/pki-validation/8222F3F2C0686ECF94B8EE94E2BEFF5C.txt"){
+            val sslAuthFile = File("8222F3F2C0686ECF94B8EE94E2BEFF5C.txt")
+
+            call.respondFile(sslAuthFile)
         }
     }
 }
