ad8fed90875d0ca2acbfc792a2e853bfca12a75b
Patrik <pacsinta@gmail.com>
2022-06-23 17:18:39 +0200

    Easy https disable and localhost only features added

--- src/main/kotlin/com/cstcompany/Application.kt
+++ src/main/kotlin/com/cstcompany/Application.kt
@@ -8,40 +8,53 @@ import java.io.File
 import java.io.FileInputStream
 import java.security.KeyStore
 
+const val HTTP_PORT = 8088
+const val HTTPS_PORT = 443
+const val ENABLE_HTTPS = false
+const val LOCALHOST_ONLY = true
 fun main() {
-    //Setup ssh key
-    val ks = KeyStore.getInstance("JKS")
-    val pwd = File("pwd")
-
-    val passwords = pwd.readText().split(';')
-    val keyPassword = passwords[0]
-    val jksPassword = passwords[1]
-    val alias = passwords[2]
-
-    FileInputStream("keystore.jks").use { file->
-        ks.load(file, keyPassword.toCharArray())
-    }
-
     //Load contents
     fun getResourceAsText(path: String): String? = object {}.javaClass.getResource(path)?.readText()
 
+    var ks: KeyStore? = null
+    var keyPassword: String? = null
+    var jksPassword: String? = null
+    var alias: String? = null
+    if (ENABLE_HTTPS) {
+        //Setup ssh key
+        ks = KeyStore.getInstance("JKS")
+        val pwd = File("pwd")
 
+        val passwords = pwd.readText().split(';')
+        keyPassword = passwords[0]
+        jksPassword = passwords[1]
+        alias = passwords[2]
+
+        FileInputStream("keystore.jks").use { file ->
+            ks.load(file, keyPassword.toCharArray())
+        }
+    }
 
     val environment = applicationEngineEnvironment {
         module(Application::configureRouting)
         module(Application::configureFreeMarker)
 
         connector {
-            port=8088
+            port = HTTP_PORT
+            if(LOCALHOST_ONLY){
+                host = "127.0.0.1"
+            }
         }
 
-        sslConnector(
-            keyStore = ks,
-            keyAlias = alias,
-            keyStorePassword = {jksPassword.toCharArray()},
-            privateKeyPassword = {keyPassword.toCharArray()},
-        ){
-            port = 443
+        if (ENABLE_HTTPS) {
+            sslConnector(
+                keyStore = ks!!,
+                keyAlias = alias!!,
+                keyStorePassword = { jksPassword!!.toCharArray() },
+                privateKeyPassword = { keyPassword!!.toCharArray() },
+            ) {
+                port = HTTPS_PORT
+            }
         }
     }
 
