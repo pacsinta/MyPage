2c4a371fa3c2f3dbf568c35b61539c079cfbe37c
Patrik Csikos <patrik.csikos@proton.me>
2024-10-09 23:46:27 +0200

    Update socialLinks

--- app/sociallinks.tsx
+++ app/sociallinks.tsx
@@ -10,24 +10,16 @@ export default function SocialLinks() {
 
   return (
       <div className="flex items-center justify-center space-x-8">
-          <a href="#" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
-              <i data-feather="twitter" className="text-gray-500 hover:text-gray-800 transition ease-in-out duration-500"></i>
+          <a href="https://github.com/pacsinta/" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+              <i data-feather="github" className="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
           </a>
 
-          <a href="#" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
-              <i data-feather="dribbble" className="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
+          <a href="https://www.linkedin.com/in/patrik-csik%C3%B3s-8166b02a1/" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+              <i data-feather="linkedin" className="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
           </a>
 
-          <a href="#" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
+          <a href="https://www.facebook.com/patrik.csikos.92/" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
               <i data-feather="facebook" className="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
-          </a>
-
-          <a href="#" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
-              <i data-feather="codepen" className="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
-          </a>
-
-          <a href="#" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
-              <i data-feather="at-sign" className="text-gray-500 hover:text-gray-700 transition ease-in-out duration-500"></i>
           </a>
 
           <a href="https://www.instagram.com/patrik_csikos/" className="w-16 h-16 flex items-center justify-center rounded-full hover:bg-gray-200 transition ease-in-out duration-500">
