e36888222ca83289b68e14491e1725d415652074
Patrik Csikos <patrik.csikos@proton.me>
2024-10-10 13:40:14 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,9 @@ export default function Home() {
                 <span className="w-28 h-0.5 bg-gray-300 mt-5"></span>
 
                 <div>
-                  <a href="https://github.com/pacsinta/signatureTester" className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Signiture tester</a>
+                  <a href="https://github.com/pacsinta/signatureTester" className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">
+                    <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Signiture tester</h1>
+                  </a>
 
                   <p className="font-normal text-gray-500 text-sm md:text-base">
                     This software can validate hand written signitures. <br /> 
@@ -103,9 +105,14 @@ export default function Home() {
                 <span className="w-28 h-0.5 bg-gray-300 mt-5"></span>
 
                 <div>
-                  <a href="https://github.com/pacsinta/WebHookUpdates" className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Webhook updates</a>
+                  <a href="https://github.com/pacsinta/TenisMonsters">
+                    <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Tenis monsters game</h1>
+                  </a>
 
-                  <p className="font-normal text-gray-500 text-sm md:text-base">I created a  </p>
+                  <p className="font-normal text-gray-500 text-sm md:text-base">
+                    This is a multiplayer game, where each player can control a monster. <br/>
+                    The goal is to successfuly hit the ball to the other player's field.
+                    </p>
                 </div>
               </div>
             </div>
@@ -116,11 +123,8 @@ export default function Home() {
       </section>
 
       <section className="py-10 md:py-16">
-
         <div className="container max-w-screen-xl mx-auto px-4">
-
           <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Education</h1>
-
           <p className="font-normal text-gray-500 text-xs md:text-base mb-20">Below is a summary of the places I studied</p>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -160,26 +164,9 @@ export default function Home() {
       </section>
 
       <section className="py-10 md:py-16">
-
         <div className="container max-w-screen-xl mx-auto px-4">
-
-          <div className="text-center">
-            <h1 className="font-normal text-gray-300 text-3xl md:text-6xl lg:text-7xl mb-20 md:mb-32 lg:mb-48">Please do not measure your skills in <br /> percentages!</h1>
-
-            <p className="font-medium text-gray-700 text-xs md:text-base">In my many years of experience, I use @laravel for backend projects and @vuejs for <br /> front-end projects. I’m an avid programmer, so I create designs based on the <br /> weekend @figmadesign.</p>
-          </div>
-
-        </div>
-
-      </section>
-
-      <section className="py-10 md:py-16">
-
-        <div className="container max-w-screen-xl mx-auto px-4">
-
           <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Experience</h1>
-
-          <p className="font-normal text-gray-500 text-xs md:text-base mb-20">Below is a summary of the places I studied</p>
+          <p className="font-normal text-gray-500 text-xs md:text-base mb-20">Below is a summary of the places I worked at</p>
 
           <div className="flex flex-col lg:flex-row justify-between">
             <div className="space-y-8 md:space-y-16 mb-16 md:mb-0">
@@ -202,91 +189,20 @@ export default function Home() {
 
       </section>
 
-      <section className="py-10 md:py-16">
-
-        <div className="container max-w-screen-xl mx-auto px-4">
-
-          <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Brands</h1>
-
-          <p className="font-normal text-gray-500 text-xs md:text-base mb-10 md:mb-20">Below is a summary of the places I studied</p>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <img src="assets/image/brand-1.png" alt="Image" />
-            <img src="assets/image/brand-2.png" alt="Image" />
-            <img src="assets/image/brand-3.png" alt="Image" />
-            <img src="assets/image/brand-4.png" alt="Image" />
-            <img src="assets/image/brand-5.png" alt="Image" />
-            <img src="assets/image/brand-6.png" alt="Image" />
-          </div>
-
-        </div>
-
-      </section>
-
-      <section className="py-10 md:py-16">
-
-        <div className="container max-w-screen-xl mx-auto px-4">
-
-          <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">Testimonial</h1>
-
-          <p className="font-normal text-gray-500 text-xs md:text-base mb-10 md:mb-20">Below is a summary of the places I studied</p>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div className="bg-gray-50 px-8 py-10 rounded-md">
-
-              <p className="font-normal text-gray-500 text-md mb-4">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.</p>
-
-              <h6 className="font-semibold text-gray-500 text-md">Stephan Clark <span className="font-medium text-gray-300 text-sm">- CEO at EarlyBird</span></h6>
-            </div>
-
-            <div className="bg-gray-50 px-8 py-10 rounded-md">
-
-              <p className="font-normal text-gray-500 text-md mb-4">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.</p>
-
-              <h6 className="font-semibold text-gray-500 text-md">Stephan Clark <span className="font-medium text-gray-300 text-sm">- CEO at EarlyBird</span></h6>
-            </div>
-
-            <div className="bg-gray-50 px-8 py-10 rounded-md">
-
-              <p className="font-normal text-gray-500 text-md mb-4">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.</p>
-
-              <h6 className="font-semibold text-gray-500 text-md">Stephan Clark <span className="font-medium text-gray-300 text-sm">- CEO at EarlyBird</span></h6>
-            </div>
-
-            <div className="bg-gray-50 px-8 py-10 rounded-md">
-
-              <p className="font-normal text-gray-500 text-md mb-4">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.</p>
-
-              <h6 className="font-semibold text-gray-500 text-md">Stephan Clark <span className="font-medium text-gray-300 text-sm">- CEO at EarlyBird</span></h6>
-            </div>
-
-            <div className="bg-gray-50 px-8 py-10 rounded-md">
-
-              <p className="font-normal text-gray-500 text-md mb-4">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.</p>
-
-              <h6 className="font-semibold text-gray-500 text-md">Stephan Clark <span className="font-medium text-gray-300 text-sm">- CEO at EarlyBird</span></h6>
-            </div>
-
-            <div className="bg-gray-50 px-8 py-10 rounded-md">
-
-              <p className="font-normal text-gray-500 text-md mb-4">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.</p>
-
-              <h6 className="font-semibold text-gray-500 text-md">Stephan Clark <span className="font-medium text-gray-300 text-sm">- CEO at EarlyBird</span></h6>
-            </div>
-          </div>
-
-        </div>
-
-      </section>
-
       <footer className="py-10 md:py-16 mb-20 md:mb-40 lg::mb-52">
 
         <div className="container max-w-screen-xl mx-auto px-4">
 
           <div className="text-center">
-            <h1 className="font-medium text-gray-700 text-4xl md:text-5xl mb-5">Testimonial</h1>
-
-            <p className="font-normal text-gray-400 text-md md:text-lg mb-20">I’m not currently taking on new client work but feel free to contact me for any <br /> other inquiries.</p>
+            <h1 className="font-medium text-gray-700 text-4xl md:text-5xl mb-5">About myself</h1>
+            <p className="font-normal text-md md:text-lg mb-5"> 
+              I'm currently working on my Master's degree in the Budapest University of Technology and Economics, but <br/>
+              I started programming as a kid from my dad in Excel Visual Basic. Shortly after that I was learning <br/> 
+              how to build robots in the Lego Mindstorms platform. From this point I was pulled in the amazing world of programming. <br />
+              I have a big passion in creating things that help people to care more with the things that matter and less w</p>
+            <p className="font-normal text-gray-400 text-md md:text-lg mb-20">
+              I’m not currently taking on new client work but feel free to contact me for any <br /> 
+              other inquiries. </p>
 
             <SocialLinks />
           </div>
