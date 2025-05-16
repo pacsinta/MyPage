4c76439ab2d54b80935814ca2107e3045279360b
Patrik Csikos <patrik.csikos@proton.me>
2025-05-16 18:43:15 +0200

    Fix portfolio description alignment

--- app/components /Portfolio.tsx
+++ app/components /Portfolio.tsx
@@ -11,7 +11,7 @@ function PortfolioElement({ title, description, index }: PortfolioElementProps) 
 
             <span className="w-28 h-0.5 bg-gray-300 mt-5"></span>
 
-            <div>
+            <div className="w-full">
                 <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">{title}</h1>
                 <p className="font-normal text-gray-500 text-sm md:text-base">{description}</p>
             </div>
